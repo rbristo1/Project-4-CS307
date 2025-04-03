bb6d19999bb83929ea813f386393f2061e577e79
acook73 <acook73@vols.utk.edu>
2025-04-03 14:43:26 -0400

    I added some stuff.

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,12 +1,68 @@
 // dijsktras.cpp
 #include <iostream>
-#include <vector> 
+#include <vector>
+#include <climits>
 #include <map>
+#include <queue>
+#include <algorithm>
 using namespace std;
+
+const int DIRECTIONS[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
 
 //returns total cost
 int BFS(vector<pair<int,int>> * path, vector<vector<int>> * map, int runnerStartRow, int runnerStartCol, int runnerEndRow, int runnerEndCol) {
-    return 1;
+    // map[row][column] = weight
+    // path = coords the runner moves to
+
+    vector<vector<int>> distances(map->size(), vector<int> (map[0].size(), INT_MAX)); // A vector that gives the distances
+    vector<vector<pair<int, int>>> prevNode(map->size(), vector<pair<int, int>>(map[0].size(), make_pair(-1, -1))); // A vector that keeps track of the previous nodes
+    vector<vector<bool>> visited(map->size(), vector<bool> (map[0].size(), false)); // A vector that keeps track of all of the visited nodes
+    priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> q; // Stores the distance, the row, and the column, in that order
+    int maxRow = map->size();
+    int maxCol = map[0].    size();
+    
+    visited[runnerStartRow][runnerStartCol] = true;
+    q.push(make_pair(0, make_pair(runnerStartRow, runnerStartCol)));
+
+    while (!q.empty()){
+        pair<int, int> node = q.top().second;
+        int dist = q.top().first;
+        q.pop();
+
+        int row = node.first;
+        int col = node.second;
+        int weight = (*map)[row][col];
+        int tempDist = dist + weight;
+        for (int i = 0; i < 4; i++) {
+            int tempRow = row + DIRECTIONS[i][0];
+            int tempCol = col + DIRECTIONS[i][1];
+
+
+
+            if (0 <= tempRow && tempRow < maxRow && 0 <= tempCol && tempCol < maxCol) { // Check for bounds
+                visited[tempRow][tempCol] = true;
+                if (tempDist < distances[tempRow][tempCol]){
+                    distances[tempRow][tempCol] = tempDist;
+                    prevNode[tempRow][tempCol] = make_pair(row, col);
+                    q.push(make_pair(distances[tempRow][tempCol], make_pair(tempRow, tempCol)));
+                }
+
+
+            }
+
+        }
+    }
+
+    pair<int, int> pathNodes = make_pair(runnerEndRow, runnerEndCol);
+    path->push_back(pathNodes);
+    while (pathNodes != make_pair(runnerStartRow, runnerStartCol)) {
+        pathNodes = prevNode[pathNodes.first][pathNodes.second];
+        path->push_back(pathNodes);
+    }
+    // At this point, path is backwords, so we need to reverse it.
+    reverse(path->begin(), path->end());
+
+    return distances[runnerEndRow][runnerEndCol];
 }
 
 // Main Execution
